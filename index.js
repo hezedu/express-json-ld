53ec6d65a30256ce138bc98ee5be3c602e5741d4
duwei <tianxiazhizhong@gmail.com>
2018-01-18 16:07:24 +0800

    package.json

--- index.js
+++ index.js
@@ -7,31 +7,43 @@ function getTplRender(tpl){
   if(placeholderIndex === -1){
     throw new Error(`express-json-ld: ${config.indexPath} unhave "${PLACEHOLDER}"`);
   }
-
+  
   var titleIndex = tpl.indexOf(TITLE);
   if(titleIndex === -1){
     throw new Error(`express-json-ld: ${config.indexPath} unhave "${TITLE}"`);
   }
-
+  var indent = tpl.lastIndexOf('\n', placeholderIndex);
+  indent = placeholderIndex - indent;
+  var indentStr = [];
+  for(let i = 0, len = indent; i < len; i++){
+    indentStr.push('');
+  }
+  indentStr = indentStr.join(' ');
   var str0, str1, str2, 
   isTitleBeforePlaceholder = titleIndex < placeholderIndex;
+  var render;
+
   if(isTitleBeforePlaceholder){
     str0 = tpl.substr(0, titleIndex);
     var _tmp = tpl.substr(titleIndex + TITLE.length);
-    str1 = _tmp.substr(0, placeholderIndex);
-    str2 = _tmp.substr(placeholderIndex + PLACEHOLDER.length);
-    return function(data){
+    _tmp = _tmp.split(PLACEHOLDER);
+    str1 = _tmp[0]
+    str2 = _tmp[1];
+    render = function(data){
       return str0 + data.title + str1 + data.body + str2;
     }
   }else{
     str0 = tpl.substr(0, placeholderIndex);
     var _tmp = tpl.substr(placeholderIndex + PLACEHOLDER.length);
-    str1 = _tmp.substr(0, titleIndex);
-    str2 = _tmp.substr(placeholderIndex + TITLE.length);
-    return function(data){
+    _tmp = _tmp.split(TITLE);
+    str1 = _tmp[0]
+    str2 = _tmp[1];
+    render = function(data){
       return str0 + data.body + str1 + data.title + str2;
     }
   }
+  render.indent = indentStr
+  return render;
 }
 
 function jsonLDWrap(obj){
@@ -41,21 +53,23 @@ function jsonLDWrap(obj){
 module.exports = function(config){
   const tpl = fs.readFileSync(config.indexPath, 'utf-8');
   const render = getTplRender(tpl);
+  const indent = '\n' + render.indent;
+  console.log('indent', indent.length)
   config.app.response.__proto__.jsonLD = function(data){
     if(!Array.isArray(data)){
       data = [data];
     }
-    var body = '', title = [];
+    var body = [], title = [];
     for(let i = 0, len = data.length; i < len; i++){
       var v = data[i]
-      body += jsonLDWrap(v);
+      body.push(jsonLDWrap(v));
       const _title = v.name || v.headline
       if(_title){
         title.unshift(_title);
       }
     }
     this.send(render({
-      body: body,
+      body: body.join(indent),
       title: title.join('-')
     }));
   };
---
+++ test/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+//app.use(express.static(path.join(__dirname, 'public')));
+var JSONLD = require('../index.js');
+JSONLD({
+  app,
+  indexPath: path.join(__dirname, 'public/index.html')
+})
+app.get('/', function(req ,res, next){
+  res.jsonLD([{
+    name: 'JSON-LD',
+    '@type': 'hahaha'
+  },
+  {
+    name: '222',
+    '@type': 'ddd'
+  }])
+})
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
